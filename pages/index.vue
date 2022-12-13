52a138dbd4f86b78859462a201e91447d04ec287
Konstantin BIFERT <hello@kissu.io>
2022-12-13 19:17:09 +0100

    done

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useIntervalFn } from '@vueuse/core' // VueUse helper, install it
+
+const { pending, data, error, refresh } = await useFetch('https://jsonplaceholder.typicode.com/todos/1')
+
+useIntervalFn(() => {
+  console.log(`refreshing the data again ${new Date().toISOString()}`)
+  refresh() // will call the 'todos' endpoint, just above
+}, 3000) // call it back every 3s
+</script>
+
+<template>
+  <div v-if="!pending">
+    <pre>{{ data }}</pre>
+  </div>
+</template>
